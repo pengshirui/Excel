7f639e158c17baa57ffa59ed4681ce4dcc7f355c
Shirui Peng <shirui.peng.822@gmail.com>
2017-12-05 22:53:01 -0500

    update

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require('webpack');
+const path = require('path');
+
+const SRC_DIR = path.resolve(__dirname, './src/excel')
+const DIST_DIR = path.resolve(__dirname, './dist')
+
+module.exports = {
+    context: __dirname,
+    devtool: debug ? "inline-source-map" : null,
+    cache: true,
+    entry: SRC_DIR + "/index.jsx",
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015', 'stage-0']
+            }
+        }]
+    },
+    output: {
+        path: DIST_DIR,
+        filename: "app.min.js"
+    },
+    plugins: debug ? [] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+    ],
+};
