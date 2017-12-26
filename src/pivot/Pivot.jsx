ecfd46bfa8a4ed1e76bf28cd37705433da50bb86
shirui.peng.822@gmail.com <1am@89Psr822>
2017-12-26 15:18:36 -0500

    Make left right panel

--- src/pivot/Pivot.jsx
+++ src/pivot/Pivot.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, FormGroup, Grid, PanelGroup } from 'react-bootstrap';
+import { Button, Col, FormGroup, Grid, PanelGroup, Row } from 'react-bootstrap';
 import { checkPattern, convertStrToArr, convertToBigSmall } from '../data/PatternService.js';
 import { compose, withHandlers, withState } from 'recompose';
 import { BallData } from '../share/BallData.jsx';
@@ -48,20 +48,26 @@ const component = (props) => {
   const disabled = !regex.test(args) || !regex.test(data);
   return (
     <Grid fluid={true}>
-      <form>
-        <FieldGroup label="数据" onChange={updateData} validationState={getValidationState(data)} tip="数字用逗号分割" />
-        <PanelGroup>
-          <BallData b={binaryData} header="二进制数据" eventKey={0} bsStyle="success"/>
-        </PanelGroup>
-        <FieldGroup label="模板" onChange={updateArgs} validationState={getValidationState(args)} tip="数字用逗号分割"/> 
-        <FieldGroup label="分隔值" onChange={updatePivot} type="number" validationState={getValidationState(pivot)} />       
-        <FormGroup>
-          <Button onClick={submit} block={true} bsStyle="primary" disabled={disabled}>计算</Button>
-        </FormGroup>
-        <PanelGroup>
-          <BallData b={result} header="结果" eventKey={0} bsStyle="success"/>
-        </PanelGroup>
-      </form>
+      <Row>
+        <Col xs={6}>
+          <form>
+            <FieldGroup label="数据" onChange={updateData} validationState={getValidationState(data)} tip="数字用逗号分割" />
+            <PanelGroup>
+              <BallData b={binaryData} header="二进制数据" eventKey={0} bsStyle="success"/>
+            </PanelGroup>
+            <FieldGroup label="模板" onChange={updateArgs} validationState={getValidationState(args)} tip="数字用逗号分割"/> 
+            <FieldGroup label="分隔值" onChange={updatePivot} type="number" validationState={getValidationState(pivot)} />       
+            <FormGroup>
+              <Button onClick={submit} block={true} bsStyle="primary" disabled={disabled}>计算</Button>
+            </FormGroup>
+          </form>
+        </Col>
+        <Col xs={6}>
+          <PanelGroup>
+            <BallData b={result} header="结果" eventKey={0} bsStyle="success"/>
+          </PanelGroup>
+        </Col>
+      </Row>
     </Grid>
   );
 }
