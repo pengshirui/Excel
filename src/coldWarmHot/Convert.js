1e3efc28ecd1fce4a18062af76a878fd79f4bc6a
Wenxin Zhao <z404617657@gmail.com>
2017-12-27 23:53:07 -0500

    add coldwarmhot algorithm

--- src/coldWarmHot/Convert.js
+++ src/coldWarmHot/Convert.js
@@ -1,13 +1,21 @@
-import { getCol } from '../util/Array.js';
-
-
 export const convertToColdWarmHot = (ballNumber, csv) => {
   let res = [];
-  let selectedBallList = csv[ballNumber]
+  for (let x = 0; x < 10; x++) res.push(0);
   for (let i = 10; i < csv.length; i++) {
-    let num = (csv[i][ballNumber - 1]);
-    for (let j = 0; j < 10; j++) {
-      
+    let num = (csv[i][ballNumber]);
+    let count = 0;
+    for (let j = 10; j > 0; j--) {
+      if ((csv[i - j][0] === num) || (csv[i - j][1] === num) || (csv[i - j][2] === num) ||
+      (csv[i - j][3] === num) || (csv[i - j][4] === num) || (csv[i - j][5] === num)) {
+        count++;
+      }
+    }
+    if (count <= 1) {
+      res.push(0);
+    } else if (count === 2) {
+      res.push(1);
+    } else if (count >= 3) {
+      res.push(2);
     }
   }
   return res;
